3f2bc5525987c863d01c87bddc4b59e005ac1537
orb <orb@wix.com>
2020-08-30 17:24:34 +0300

    typescript

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = 'hello';
   
     return element;
   }
