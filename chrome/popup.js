45efec6f8e6cca7631f1a5a5795b99d4e31fd979
orb <orb@wix.com>
2020-09-17 18:10:17 +0300

    window.close

--- chrome/popup.js
+++ chrome/popup.js
@@ -9,9 +9,11 @@ function sendMessageToTabs(msg) {
 const placeAvatarsBtn = document.getElementById('placeAvatars');
 placeAvatarsBtn.onclick = function() {
     sendMessageToTabs({ action: "add_avatars" });
+    window.close();
 };
 
 const hideAvatarsBtn = document.getElementById('hideAvatars');
 hideAvatarsBtn.onclick = function() {
     sendMessageToTabs({ action: "hide_avatars" });
+    window.close();
 };
