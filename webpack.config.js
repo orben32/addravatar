69a139c03b6024f1f9755673119b9c18da6b1062
orb <orb@wix.com>
2020-09-22 15:28:41 +0300

    Use bitcoin-address-validation to support more address formats

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,40 @@ const baseManifest = require("./chrome/manifest.json");
 const WebpackExtensionManifestPlugin = require("webpack-extension-manifest-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-  module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    plugins: [
-      new WebpackExtensionManifestPlugin({
-        config: {
-          base: baseManifest
-        }
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: 'chrome/background.js', to: 'background.js' },
-          { from: 'chrome/popup.js', to: 'popup.js' },
-          { from: 'chrome/popup.html', to: 'popup.html' },
-          { from: 'chrome/manifest.json', to: 'manifest.json' },
-          { from: 'chrome/icons', to: 'icons' },
-        ],
-  
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  plugins: [
+    new WebpackExtensionManifestPlugin({
+      config: {
+        base: baseManifest
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'chrome/background.js', to: 'background.js' },
+        { from: 'chrome/popup.js', to: 'popup.js' },
+        { from: 'chrome/popup.html', to: 'popup.html' },
+        { from: 'chrome/manifest.json', to: 'manifest.json' },
+        { from: 'chrome/icons', to: 'icons' },
       ],
-    },
-    resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ],
-    },
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
