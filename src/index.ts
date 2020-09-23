1fe9b5790c627d6153cb83dcec453ac0b971ee41
Or Ben-Ari <32925515+orben32@users.noreply.github.com>
2020-09-23 16:18:15 +0300

    remove avatars on location change (#8)
    * Remove avatars on location change
    
    * avatarStore
    
    * avatarUpdates

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,17 @@
-import { findAddressNodes } from "./nodeQueries";
-import { addAvatarElement } from "./avatarElementFactory";
-import { getStyle } from './globalStyleDefinition';
+import { addAvatars, removeAvatars } from "./avatarStore";
+import { observeUpdates } from "./avatarUpdates";
 
 function createExtension() {
-  let avatars: Element[];
-  let injectedStyle: HTMLStyleElement | null = null;
-
-  function removeAvatars() {
-    if (avatars) {
-      for (const element of avatars) {
-        element.parentElement.removeChild(element);
-      }
-      avatars = null;
-    }
-
-    if (injectedStyle) {
-      injectedStyle.parentElement.removeChild(injectedStyle);
-      injectedStyle = null;
-    }
-  }  
-
-  const injectGlobalStyle = () => {
-    injectedStyle = document.createElement('style');
-    injectedStyle.innerHTML = getStyle();
-    const ref = document.querySelector('script');
-    ref.parentNode.insertBefore(injectedStyle, ref);
-  }
-
-  const addAvatars = function() {
-    removeAvatars();
-    injectGlobalStyle();
-    
-    const nodes = findAddressNodes(document.body);
-    avatars = nodes.map(node => addAvatarElement(node));
-  };
-
   window.chrome.runtime.onMessage.addListener((msg) => {
     if (msg.action == 'add_avatars') {
       addAvatars();
     } else if (msg.action == 'hide_avatars') {
       removeAvatars();  
     }
+  });
+
+  window.addEventListener('load', () => {
+    observeUpdates();    
   });
 }
 
